c8591bf84cc22b9c957ccc2b8f1e483df2b34ef1
Nuwan-Wi <nuwanwijeweera97@gmail.com>
2022-02-20 13:01:12 +0530

    Define Methods

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
+/*create: 2022 02 20
+Owner: Nuwan Wijeweera */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    /**this is a simple print
+    */
 }
---
+++ Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome { // this define the class
+    public static void main(String[] args) { // this define the main metod
+        System.out.print("Welcome to "); // print in multiple statesment
+        System.out.println("java Programing");
+        printnext(); // call another method
+    }// end of main method
+
+    public static void printnext() {        //define another method
+        System.out.println("Print this Using another method in java.");
+    }       //end the printnext method
+
+} // end of Welcome class
