19f2e6687d087d3f12474f7fa806a5574271c01b
Nuwan-Wi <nuwanwijeweera97@gmail.com>
2022-03-14 20:54:37 +0530

    add some changes

--- RandomNUmbers.java
+++ RandomNUmbers.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class RandomNUmbers {
     public static void main(String[] args) {
         Random Randnum = new Random();
+        int maxnum = 100;
 
         // int num1 = Randnum.nextInt(100);
         // int num2 = Randnum.nextInt(100);
@@ -17,9 +18,18 @@ public class RandomNUmbers {
         //     System.out.printf("The largest generate number is %d", num2);
 
         int [] number = new int[10];
-        int min = Integer.MIN_VALUE;
-        
-        System.out.println("Random numbers are "+ min);
+        for (int i=0; i<number.length; i++){
+            int num = Randnum.nextInt(89)+10;
+            number[i] = num;
+        }
 
+        for (int j=0; j<number.length; j++){
+            System.out.println(number[j]);
+            if (maxnum>number[j])
+                maxnum = number[j];        
+
+        }
+        System.out.println("maximum number = "+maxnum);
     }
+
 }
