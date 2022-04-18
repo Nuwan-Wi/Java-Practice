001ce563cd7d80d435a910c25a65c2d0a73227f7
Nuwan-Wi <nuwanwijeweera97@gmail.com>
2022-04-18 22:58:42 +0530

    add comments

--- Account.java
+++ Account.java
@@ -1,10 +1,12 @@
+/*create: 2022 04 14
+Owner: Nuwan Wijeweera */
 public class Account {
-    private String name;
+    private String name;       //declared a instance variable
 
-    public void setName(String name){
+    public void setName(String name){   //set method
         this.name = name;
     }
-    public String getName(){
+    public String getName(){    //get method
         return name;
     }
     
