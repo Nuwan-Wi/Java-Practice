4fac2845bb1b8842efd43b361a89991944fe5ae4
Nuwan-Wi <nuwanwijeweera97@gmail.com>
2022-04-18 22:52:39 +0530

    add comments

--- AccountTest.java
+++ AccountTest.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
+
+/*create: 2022 04 14
+Owner: Nuwan Wijeweera */
+import java.util.Scanner; //import scanner class
 
 public class AccountTest {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // create object to use scanner class
 
-        Account myAccount = new Account();
+        Account myAccount = new Account(); // create object to use Account class in Account.java (both in same pacage.
+                                           // so didn't want to import.)
 
-        System.out.print("Pleace enter the name: ");
-        String theName = input.nextLine();
-        myAccount.setName(theName);
+        System.out.print("Pleace enter the name: "); // get the name as input
+        String theName = input.nextLine(); // asign input into variable
+        myAccount.setName(theName); // set the paramiter into object
 
-        System.out.printf("Name in  object myAccount is %s.", myAccount.getName());
+        System.out.printf("Name in  object myAccount is %s.", myAccount.getName()); // get output
+
+        input.close(); // close clanner object
 
     }
 }
