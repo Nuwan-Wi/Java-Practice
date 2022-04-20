cc9c41f262856afbc99425e82332e2c300b7e0a8
Nuwan-Wi <nuwanwijeweera97@gmail.com>
2022-04-21 00:19:52 +0530

    use constructor

--- Account.java
+++ Account.java
@@ -3,6 +3,10 @@ Owner: Nuwan Wijeweera */
 public class Account {
     private String name;       //declared a instance variable
 
+    //this constructor class use only when need to pass paramiters directly
+    public Account (String name){   //this class useless if paramiters get from input statements.
+        this.name = name;
+    }
     public void setName(String name){   //set method
         this.name = name;
     }
--- AccountTest.java
+++ AccountTest.java
@@ -7,18 +7,23 @@ public class AccountTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); // create object to use scanner class
 
-        Account myAccount = new Account(); // create object to use Account class in Account.java (both in same pacage.
+        /**
+         Account myAccount = new Account(); // create object to use Account class in Account.java (both in same pacage.
                                            // so didn't want to import.)
 
         System.out.print("Pleace enter the name: "); // get the name as input
         String theName = input.nextLine(); // asign input into variable
         myAccount.setName(theName); // set the paramiter into object
 
-        System.out.printf("Name in  object myAccount is %s.", myAccount.getName()); // get output
+        System.out.printf("Name in object myAccount is %s.%n", myAccount.getName()); // get output
+        */
 
         // Create two object that provide paramiters
-        Account account1 = new Account();
-        Account account2 = new Account();
+        Account account1 = new Account("Nuwi wijeweera");
+        Account account2 = new Account("Nuwanya rathnashsri");
+
+        System.out.printf("Name in object Account is %s.%n", account1.getName());
+        System.out.printf("Name in object mAccount is %s.%n", account2.getName());
 
         input.close(); // close clanner object
 
