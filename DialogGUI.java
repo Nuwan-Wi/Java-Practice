45b6885e0436bc8c22add3e2a2f7b6292a0dad6a
Nuwan-Wi <nuwanwijeweera97@gmail.com>
2022-08-25 22:58:52 +0530

    java swing GUI

---
+++ DialogGUI.java
@@ -0,0 +1,10 @@
+/*create: 2022 08 25
+Owner: Nuwan Wijeweera */
+
+import javax.swing.JOptionPane;
+
+public class DialogGUI {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "welcome");
+    }
+}
